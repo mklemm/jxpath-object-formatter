514cfbfb45f7ad60aa9e17bda233750a0a91fcd3
Mirko Klemm <mirko@cm-klemm.de>
2016-05-04 17:48:26 +0200

    Added coalesce() and hash() extension functions

--- src/main/java/net/codesup/jxpath/formatter/JXPathFormatExtension.java
+++ src/main/java/net/codesup/jxpath/formatter/JXPathFormatExtension.java
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package net.codesup.jxpath.formatter;
 
 import java.text.MessageFormat;
@@ -39,14 +38,15 @@ public final class JXPathFormatExtension {
 	public static String isoDate(final Date date) {
 		return date(date, "yyyy-MM-dd");
 	}
+
 	public static String isoDateTime(final Date date) {
 		return date(date, "yyyy-MM-dd'T'HH:MM:ssZ");
 	}
 
 	private static Object decode(final Object ref, final Object[] keyValues) {
-		final int hasDef =  keyValues.length % 2 == 0 ? 0 : 1;
+		final int hasDef = keyValues.length % 2 == 0 ? 0 : 1;
 		final Object def = keyValues.length == 0 || hasDef == 0 ? null : keyValues[keyValues.length - 1];
-		if(ref == null) {
+		if (ref == null) {
 			for (int i = 0; i < keyValues.length - hasDef; i += 2) {
 				final Object key = keyValues[i];
 				final Object val = keyValues[i + 1];
@@ -67,37 +67,84 @@ public final class JXPathFormatExtension {
 		}
 	}
 
-	public static Object decode(Object ref, Object key1, Object val1) {
+	private static Object coalesce(final Object[] refs) {
+		for (final Object ref : refs) {
+			if (ref != null) return ref;
+		}
+		return null;
+	}
+
+
+	public static Object decode(final Object ref, final Object key1, final Object val1) {
 		return decode(ref, new Object[]{val1, key1, val1});
 	}
-	public static Object decode(Object ref, Object key1, final Object val1, final Object def) {
+
+	public static Object decode(final Object ref, final Object key1, final Object val1, final Object def) {
 		return decode(ref, new Object[]{key1, val1, def});
 	}
-	public static Object decode(Object ref, Object key1, Object val1, final Object key2, final Object val2) {
+
+	public static Object decode(final Object ref, final Object key1, final Object val1, final Object key2, final Object val2) {
 		return decode(ref, new Object[]{val1, key1, val1, key2, val2});
 	}
-	public static Object decode(Object ref, Object key1, Object val1, final Object key2, final Object val2, final Object def) {
+
+	public static Object decode(final Object ref, final Object key1, final Object val1, final Object key2, final Object val2, final Object def) {
 		return decode(ref, new Object[]{val1, key1, val1, key2, val2, def});
 	}
-	public static Object decode(Object ref, Object key1, Object val1, final Object key2, final Object val2, final Object key3, final Object val3) {
+
+	public static Object decode(final Object ref, final Object key1, final Object val1, final Object key2, final Object val2, final Object key3, final Object val3) {
 		return decode(ref, new Object[]{val1, key1, val1, key2, val2, key3, val3});
 	}
-	public static Object decode(Object ref, Object key1, Object val1, final Object key2, final Object val2, final Object key3, final Object val3, final Object def) {
+
+	public static Object decode(final Object ref, final Object key1, final Object val1, final Object key2, final Object val2, final Object key3, final Object val3, final Object def) {
 		return decode(ref, new Object[]{val1, key1, val1, key2, val2, key3, val3, def});
 	}
-	public static Object decode(Object ref, Object key1, Object val1, final Object key2, final Object val2, final Object key3, final Object val3, final Object key4, final Object val4) {
+
+	public static Object decode(final Object ref, final Object key1, final Object val1, final Object key2, final Object val2, final Object key3, final Object val3, final Object key4, final Object val4) {
 		return decode(ref, new Object[]{val1, key1, val1, key2, val2, key3, val3, key4, val4});
 	}
-	public static Object decode(Object ref, Object key1, Object val1, final Object key2, final Object val2, final Object key3, final Object val3, final Object key4, final Object val4, final Object def) {
+
+	public static Object decode(final Object ref, final Object key1, final Object val1, final Object key2, final Object val2, final Object key3, final Object val3, final Object key4, final Object val4, final Object def) {
 		return decode(ref, new Object[]{val1, key1, val1, key2, val2, key3, val3, key4, val4, def});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1) {
+		return coalesce(new Object[]{ref0, ref1});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1, final Object ref2) {
+		return coalesce(new Object[]{ref0, ref1, ref2});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1, final Object ref2, final Object ref3) {
+		return coalesce(new Object[]{ref0, ref1, ref2, ref3});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1, final Object ref2, final Object ref3, final Object ref4) {
+		return coalesce(new Object[]{ref0, ref1, ref2, ref3, ref4});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1, final Object ref2, final Object ref3, final Object ref4, final Object ref5) {
+		return coalesce(new Object[]{ref0, ref1, ref2, ref3, ref4, ref5});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1, final Object ref2, final Object ref3, final Object ref4, final Object ref5, final Object ref6) {
+		return coalesce(new Object[]{ref0, ref1, ref2, ref3, ref4, ref5, ref6});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1, final Object ref2, final Object ref3, final Object ref4, final Object ref5, final Object ref6, final Object ref7) {
+		return coalesce(new Object[]{ref0, ref1, ref2, ref3, ref4, ref5, ref6, ref7});
+	}
+
+	public static Object coalesce(final Object ref0, final Object ref1, final Object ref2, final Object ref3, final Object ref4, final Object ref5, final Object ref6, final Object ref7, final Object ref8) {
+		return coalesce(new Object[]{ref0, ref1, ref2, ref3, ref4, ref5, ref6, ref7, ref8});
 	}
 
 	private static String flattenFormat(final String pattern, final Object... params) {
 		final Object[] convertedParams = new Object[params.length];
-		for(int i = 0; i < params.length; i++) {
-			if(params[i] instanceof List) {
+		for (int i = 0; i < params.length; i++) {
+			if (params[i] instanceof List) {
 				final List<?> list = (List<?>)params[i];
-				if(!list.isEmpty()) {
+				if (!list.isEmpty()) {
 					convertedParams[i] = list.get(0);
 				} else {
 					convertedParams[i] = params[i];
@@ -112,23 +159,32 @@ public final class JXPathFormatExtension {
 	public static String format(final String pattern, final Object val) {
 		return flattenFormat(pattern, val);
 	}
+
 	public static String format(final String pattern, final Object val1, final Object val2) {
 		return flattenFormat(pattern, val1, val2);
 	}
+
 	public static String format(final String pattern, final Object val1, final Object val2, final Object val3) {
 		return flattenFormat(pattern, val1, val2, val3);
 	}
+
 	public static String format(final String pattern, final Object val1, final Object val2, final Object val3, final Object val4) {
 		return flattenFormat(pattern, val1, val2, val3, val4);
 	}
+
 	public static String format(final String pattern, final Object val1, final Object val2, final Object val3, final Object val4, final Object val5) {
 		return flattenFormat(pattern, val1, val2, val3, val4, val5);
 	}
+
 	public static String format(final String pattern, final Object val1, final Object val2, final Object val3, final Object val4, final Object val5, final Object val6) {
 		return flattenFormat(pattern, val1, val2, val3, val4, val5, val6);
 	}
 
 	public static String date(final Date date, final String pattern) {
 		return new SimpleDateFormat(pattern).format(date);
+	}
+
+	public static int hash(final Object o) {
+		return o == null ? 0 : o.hashCode();
 	}
 }
